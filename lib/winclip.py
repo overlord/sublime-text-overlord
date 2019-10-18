c56c4a7f584e40cb6ea75695a9e02e0550b595fc
Overlord <overlord1983@mail.ru>
2019-10-18 17:40:26 +0300

    [st] fix winclip on win 10

--- lib/winclip.py
+++ lib/winclip.py
@@ -4,55 +4,64 @@
 # Based off of http://stackoverflow.com/a/3429034/334717
 # and http://pywin32.hg.sourceforge.net/hgweb/pywin32/pywin32/file/4c7503da2658/win32/src/win32clipboardmodule.cpp
 # ------------------------------------------------------------------------------------------
-import sys
 import ctypes
+import sys
+import time
 # ------------------------------------------------------------------------------------------
 # привет, я русский буков
 GHND = 0x0042
+GMEM_MOVEABLE = 0x0002
 # ------------------------------------------------------------------------------------------
-msvcrt = ctypes.cdll.msvcrt
 kernel32 = ctypes.windll.kernel32
+msvcrt = ctypes.CDLL('msvcrt')
 user32 = ctypes.windll.user32
 # ------------------------------------------------------------------------------------------
 CF_TEXT = 1
 CF_UNICODETEXT = 13
-CF_HTML  = user32.RegisterClipboardFormatA(b"HTML Format")
-CF_RTF   = user32.RegisterClipboardFormatA(b"Rich Text Format")
+CF_HTML = user32.RegisterClipboardFormatA(b"HTML Format")
+CF_RTF = user32.RegisterClipboardFormatA(b"Rich Text Format")
 CF_RTFWO = user32.RegisterClipboardFormatA(b"Rich Text Format Without Objects")
 # ------------------------------------------------------------------------------------------
 TRACE = True
-
 # ------------------------------------------------------------------------------------------
 def printd(value):
 	if TRACE:
-		print(value)
-
+		print("[WINCLIP] %s\n" % value)
 # ------------------------------------------------------------------------------------------
-def last_error(label):
-	code = ctypes.GetLastError()
-	msg = ctypes.FormatError(code)
-	if code == 0:
-		msg = ''
-	# printd("%s: %s '%s'" % (label, code, msg))
-
+def OpenClipboard(hwnd):
+	# We may not get the clipboard handle immediately because
+	# some other application is accessing it (?)
+	# We try for at least 500ms to get the clipboard.
+	t = time.time() + 0.5
+	success = False
+	while time.time() < t:
+		success = user32.OpenClipboard(hwnd)
+		if success:
+			break
+		time.sleep(0.01)
+		if not success:
+			raise Exception("Error calling OpenClipboard")
 # ------------------------------------------------------------------------------------------
 def Get():
 	try:
-		user32.OpenClipboard(None)
+		hwnd = user32.CreateWindowExA(0, b"STATIC", None, 0, 0, 0, 0, 0, None, None, None, None)
+		OpenClipboard(hwnd)
 		pcontents = user32.GetClipboardData(CF_TEXT)
 		return ctypes.c_char_p(pcontents).value
 	finally:
 		user32.CloseClipboard()
+		user32.DestroyWindow(hwnd)
 # ------------------------------------------------------------------------------------------
 def Paste(data, paste_type='text', plaintext=None):
 
-	# printd("type(Paste.data) = %s" % type(data))
+	printd("type(Paste.data): %s" % type(data))
 
 	if plaintext is None:
 		plaintext = data
 
 	try:
-		user32.OpenClipboard(None)
+		hwnd = user32.CreateWindowExA(0, b"STATIC", None, 0, 0, 0, 0, 0, None, None, None, None)
+		OpenClipboard(hwnd)
 		user32.EmptyClipboard()
 
 		if paste_type == 'rtf':
@@ -65,6 +74,7 @@ def Paste(data, paste_type='text', plaintext=None):
 		Put(plaintext, CF_UNICODETEXT, 'utf_16')
 	finally:
 		user32.CloseClipboard()
+		user32.DestroyWindow(hwnd)
 
 # ------------------------------------------------------------------------------------------
 def to_bytes(data, codepage):
@@ -77,29 +87,35 @@ def to_bytes(data, codepage):
 # ------------------------------------------------------------------------------------------
 def Put(data, clipboard_format, codepage):
 
-	# printd("------------------------------")
-	# printd("%s - type(Put.data) = %s" % (clipboard_format, type(data)))
-	# printd("repr(Put.data) = %s" % repr(data))
+	printd("%s - type(Put.data) = %s" % (clipboard_format, type(data)))
+	printd("repr(Put.data) = %s" % repr(data))
 
 	b_data = to_bytes(data, codepage)
 
-	# printd("type(Put.b_data) = %s" % type(b_data))
-	# printd("repr(Put.b_data) = %s" % repr(b_data))
+	printd("type(Put.b_data) = %s" % type(b_data))
+	printd("repr(Put.b_data) = %s" % repr(b_data))
 
-	if clipboard_format in (CF_UNICODETEXT, ):
-		handle = Put2(b_data, ctypes.c_wchar, ctypes.c_wchar_p, msvcrt.wcscpy)
-	else:
-		handle = Put2(b_data, ctypes.c_char, ctypes.c_char_p, msvcrt.strcpy)
+	size = len(b_data) + ctypes.sizeof(ctypes.c_wchar)
+	handle = kernel32.GlobalAlloc(GMEM_MOVEABLE, size)
+	locked_handle = kernel32.GlobalLock(handle)
+	printd("codepage: %s; clipboard_format: %s; size: %s; handle: %s; locked_handle: %s" % (codepage, clipboard_format, size, handle, locked_handle))
+	msvcrt.wcscpy(ctypes.c_wchar_p(locked_handle), b_data)
 
-	user32.SetClipboardData(ctypes.c_int(clipboard_format), handle)
+	# if clipboard_format in (CF_UNICODETEXT, ):
+	# 	size = len(b_data) + ctypes.sizeof(ctypes.c_wchar)
+	# 	handle = kernel32.GlobalAlloc(GMEM_MOVEABLE, size)
+	# 	locked_handle = kernel32.GlobalLock(handle)
+	# 	printd("codepage: %s; clipboard_format: %s; size: %s; handle: %s; locked_handle: %s" % (codepage, clipboard_format, size, handle, locked_handle))
+	# 	msvcrt.wcscpy(ctypes.c_wchar_p(locked_handle), b_data)
+	# else:
+	# 	size = len(b_data) + ctypes.sizeof(ctypes.c_char)
+	# 	handle = kernel32.GlobalAlloc(GMEM_MOVEABLE, size)
+	# 	locked_handle = kernel32.GlobalLock(handle)
+	# 	printd("codepage: %s; clipboard_format: %s; size: %s; handle: %s; locked_handle: %s" % (codepage, clipboard_format, size, handle, locked_handle))
+	# 	msvcrt.strcpy(ctypes.c_char_p(locked_handle), b_data)
 
-def Put2(b_data, char_type, char_type_pointer, char_copy_function):
-	size = len(b_data) + ctypes.sizeof(char_type())
-	handle = kernel32.GlobalAlloc(GHND, size)
-	pointer = kernel32.GlobalLock(handle)
-	char_copy_function(char_type_pointer(pointer), b_data)
 	kernel32.GlobalUnlock(handle)
-	return handle
+	user32.SetClipboardData(ctypes.c_int(clipboard_format), handle)
 
 # ------------------------------------------------------------------------------------------
 # Based off of http://code.activestate.com/recipes/474121-getting-html-from-the-windows-clipboard/
@@ -126,8 +142,6 @@ EndFragment:%09d'''
 	endFragment = startFragment + fragment_length
 
 	result = METADATA % (startHtml, endHtml, startFragment, endFragment) + CONTEXT_BEGIN + fragment + CONTEXT_END
-
-	# printd(result)
 
 	return result
 # ------------------------------------------------------------------------------------------
