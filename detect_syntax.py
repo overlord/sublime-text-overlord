716618cb2d6718e89eb6a18aa4de700e43f3993f
Overlord <overlord1983@mail.ru>
2022-09-20 12:25:50 +0300

    st_overlord - detect json

--- detect_syntax.py
+++ detect_syntax.py
@@ -4,22 +4,28 @@ import sublime_plugin
 # ------------------------------
 import re
 # ------------------------------
-
 LOOK_BACK, LOOK_FORTH = 0, 10
+
+JSON = r'(?xi) ( " \s* : \s* " | \[ \s* { \s* " | " \s* : \s* \[ )'
+POWERSHELL = r'(?i)(\$\w+|\[(\w+\.)*\w+\]::\w+|foreach\s*\{|@[(]|\s+-\w+|<[#]|[#]>)'
+PYTHON = r'(^\s*(def|class))|(\):\s*)'
+SQL = r'(?i)\b(select|update|from|inner|where|join)\b'
+XML = r'<(/)?(\w+:)?\w+(\s+(\w+:)?\w+\s*=\s*"[^"]*")*(\s+/)?>'
+
 SYNTAXES = [
-	(r'<(/)?(\w+:)?\w+(\s+(\w+:)?\w+\s*=\s*"[^"]*")*(\s+/)?>',                'Packages/XML/XML.tmLanguage'),
-	(r'(?i)(\$\w+|\[(\w+\.)*\w+\]::\w+|foreach\s*\{|@[(]|\s+-\w+|<[#]|[#]>)', 'Packages/PowerShell/Support/PowershellSyntax.tmLanguage'),
-	(r'(^\s*(def|class))|(\):\s*)',                                           'Packages/Python/Python.tmLanguage'),
-	(r'(?i)\b(select|update|from|inner|where|join)\b',                        'Packages/sublime_overlord/syntaxes/SQL Ex.tmLanguage'),
+	(XML,        'Packages/XML/XML.tmLanguage'),
+	(POWERSHELL, 'Packages/PowerShell/Support/PowershellSyntax.tmLanguage'),
+	(PYTHON,     'Packages/Python/Python.tmLanguage'),
+	(SQL,        'Packages/sublime_overlord/syntaxes/SQL Ex.tmLanguage'),
+	(JSON,       'Packages/JSON/JSON.tmLanguage'),
 ]
 
 # ------------------------------
-
 class overlord_detect_syntax(sublime_plugin.TextCommand):
 
 	def detect_syntax(self, view):
 		point = view.sel()[0].a
-		(row, col) = view.rowcol(point)
+		row, col = view.rowcol(point)
 
 		region = view.line(sublime.Region(view.text_point(row - LOOK_BACK, 0), view.text_point(row + LOOK_FORTH, 0)))
 		s = view.substr(region)
@@ -31,6 +37,7 @@ class overlord_detect_syntax(sublime_plugin.TextCommand):
 				selected_syntax, count = syntax, qty
 
 		if count > 0:
+			print('Detected: %s' % (selected_syntax))
 			view.set_syntax_file(selected_syntax)
 
 		return region
@@ -38,4 +45,3 @@ class overlord_detect_syntax(sublime_plugin.TextCommand):
 	def run(self, edit):
 		region = self.detect_syntax(self.view)
 
-# ------------------------------
